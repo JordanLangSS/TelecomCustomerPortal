da93d609b7f0ec0be9353ca0d0f061cfd110b3bb
Jordan Lang <jlang@skillstorm.com>
2022-07-20 14:58:02 -0400

    add Billing cards and Add Plan Functionality

--- FrontEnd/src/app/billing/billing.component.ts
+++ FrontEnd/src/app/billing/billing.component.ts
@@ -1,6 +1,7 @@
 import { DataSource } from '@angular/cdk/collections';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 import { Plan } from '../Response/plans';
@@ -13,44 +14,10 @@ import { PlanService } from '../service/plan.service';
 })
 
 export class BillingComponent implements OnInit {
-
-  //dataSource = new PlanDataSource(this.planService)
-  //initialize and pass planService Object
-  // dataSource = new MatTableDataSource<Plan>();
-  // displayedColumns: string[] = ['name', 'deviceLimit', 'price'];
-  // constructor(private planService: PlanService) {}
-
-  
-  public plansList: Plan[];
-  constructor(private planService: PlanService) {}
-
-  public getPlans(): void {
-    this.planService.getPlans().subscribe(
-      (response: Plan[]) => {
-        this.plansList = response;
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    )
-  }
+ 
 
   ngOnInit(): void {
-    this.getPlans();
-    // this.planService.getPlans().subscribe(
-    //   data => this.dataSource.data = data
-    //   );
+
   }
 
 }
-// export class PlanDataSource extends DataSource<any> {
-//   constructor(private planService: PlanService) {
-//     super();
-//    }
-//   connect(): Observable<Plan[]> {
-//     // return observable of type Plan[]
-//     return this.planService.getPlans();
-//   }
-//   disconnect() {}
-// }
-
