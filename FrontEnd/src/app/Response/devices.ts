9b8f516c3688ab8f757fb7e33a9a231cde0cdf30
Jordan Lang <jlang@skillstorm.com>
2022-07-22 22:25:28 -0400

    fix object output for phone number

--- FrontEnd/src/app/Response/devices.ts
+++ FrontEnd/src/app/Response/devices.ts
@@ -2,6 +2,6 @@ export interface Devices{
     id: number;
     make: string;
     model: string;
-    phoneNumber: object;
-    phoneNumbers: string;
+    phoneNumber: any;
+    phoneNumbers: any;
 }
